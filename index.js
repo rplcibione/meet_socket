9abcce0888a00fd7582c408b49c10471f304904b
exairie <rnugraha305@gmail.com>
2020-05-02 20:41:53 +0700

    .

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ io.on("connect", (socket) => {
       console.log(roomId);
       console.log(chatInfo);
       console.log("Sending to room" + roomId);
-      io.sockets.emit("chat", chatInfo);
+      socket.broadcast.emit("chat", chatInfo);
 
       socket.emit("sys_notif", `Message broadcasted`);
     } catch (error) {
